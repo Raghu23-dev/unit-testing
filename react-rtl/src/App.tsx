d35f2237905568daf6b2e58bffe5b4ceb35e7acd
raghu23-dev <raghu2308.dev@gmail.com>
2024-08-20 11:26:12 +0530

    testing the components (application component)

---
+++ react-rtl/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Application from './components/application/application';
+
+
+//npm run test -- --coverage .
+
+
+function App() {
+  return (
+    <div className="App">
+      <Application/>
+    </div>
+  );
+}
+
+export default App;
