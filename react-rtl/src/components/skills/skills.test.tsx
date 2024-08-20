4c9cc2409223da7e3c2ae289507871f952fbcdfb
raghu23-dev <raghu2308.dev@gmail.com>
2024-08-20 14:35:50 +0530

    testing start learning button (queryByRole)

--- react-rtl/src/components/skills/skills.test.tsx
+++ react-rtl/src/components/skills/skills.test.tsx
@@ -5,13 +5,26 @@ describe("skills", () => {
   const skills = ["HTML", "CSS", "JAVASCRIPT"];
   test("renders correctly", () => {
     render(<Skills skills={skills} />);
-    const listElement = screen.getByRole("list");  //ul
+    const listElement = screen.getByRole("list"); //ul
     expect(listElement).toBeInTheDocument();
   });
 
   test("renders alist of skills", () => {
     render(<Skills skills={skills} />);
-    const listItemElements = screen.getAllByRole("listitem") //li
-    expect(listItemElements).toHaveLength(skills.length)
+    const listItemElements = screen.getAllByRole("listitem"); //li
+    expect(listItemElements).toHaveLength(skills.length);
+  });
+
+  test("renders login button", () => {
+    render(<Skills skills={skills} />);
+    const loginButton = screen.queryByRole("button", { name: "Login" });
+    expect(loginButton).toBeInTheDocument();
+  });
+  test("start learning button is n't rendered", () => {
+    render(<Skills skills={skills} />);
+    const startLearningButton = screen.queryByRole("button", {
+      name: "start learning",
+    });
+    expect(startLearningButton).not.toBeInTheDocument();
   });
 });
